d8183881a124bdbcc717f685cc337333025b3f28
Swapnil Tiwari <swaptiwari@outlook.com>
2022-05-08 11:34:39 +0530

    Rectifying errors

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import { createFile, instantiateSmartContract, stateVariableUpdate, queryChanges } from './hbarsdk'
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 
 function App()
 {
+    const [contractId, funcContractId] = useState()
     const [getVariable, funcGetVariable] = useState()
     const [setVariable, funcSetVariable] = useState()
 
-    const createContractId = async function ()
+    async function createContractId()
     {
         const bytecodeFileId = await createFile()
         const settingId = await instantiateSmartContract(bytecodeFileId)
-        return settingId
+        funcContractId(settingId)
     }
 
     async function settingVariable()
     {
-        const _temp = await stateVariableUpdate(createContractId, "Alive", 1111)
+        const _temp = await stateVariableUpdate(contractId, "Alice", 1111)
         funcSetVariable(_temp)
     }
 
     async function gettingVariable()
     {
-        const _temp = await queryChanges(createContractId, "Alice")
+        const _temp = await queryChanges(contractId, "Alice")
         funcGetVariable(_temp)
     }
 
 
     return (
-        <div>
+
+        <div >
             <h1>The Hedera Getter Setter</h1>
-            <p> {setVariable}</p>
+            <button onClick={createContractId}> Create contract id </button>
+            {/* <p> {setVariable}</p> */}
             <button onClick={settingVariable}> Setting the Variable </button>
             <button onClick={gettingVariable}> Getting the Variable: </button>
-            <p> {getVariable} </p>
+            {/* <p> {getVariable} </p> */}
         </div>
+
+
+
 
     )
 }
--- src/hbarsdk.js
+++ src/hbarsdk.js
@@ -1,23 +1,23 @@
 //require("dotenv").config()
-const
-    {
-        AccountId,
-        PrivateKey,
-        Client,
-        FileCreateTransaction,
-        ContractCreateTransaction,
-        ContractFunctionParameters,
-        ContractExecuteTransaction,
-        ContractCallQuery,
-        Hbar
-    } = require("@hashgraph/sdk")
+import
+{
+    AccountId,
+    PrivateKey,
+    Client,
+    FileCreateTransaction,
+    ContractCreateTransaction,
+    ContractFunctionParameters,
+    ContractExecuteTransaction,
+    ContractCallQuery,
+    Hbar
+} from "@hashgraph/sdk"
 
 
 let client;
 async function createFile()
 {
-    const operatorId = AccountId.fromString("OP_key")
-    const operatorKey = PrivateKey.fromString("PV_key")
+    const operatorId = AccountId.fromString("0.0.34194909")
+    const operatorKey = PrivateKey.fromString("302e020100300506032b657004220420f8a94c0825d5f1391f9874d71726897c580ab493bfb8bbd21a5385381f69ce78")
     client = Client.forTestnet().setOperator(operatorId, operatorKey)
     //Import the compiled bytecode
     const helloHedera = require("./Lookup.json")
@@ -32,7 +32,7 @@ async function createFile()
     const fileSubmitTx = await fileCreateSign.execute(client)
     const fileCreateReceipt = await fileSubmitTx.getReceipt(client)
     const bytecodeFileId = fileCreateReceipt.fileId
-    //console.log(`The bytecode file ID is ${bytecodeFileId} \n`)
+    console.log(`The bytecode file ID is ${bytecodeFileId} \n`)
     return bytecodeFileId
 }
 
@@ -44,11 +44,12 @@ async function instantiateSmartContract(bytecodeFileId)
     const contractInstantiateTx = new ContractCreateTransaction()
         .setBytecodeFileId(bytecodeFileId)
         .setGas(100000)
+        .setConstructorParameters(new ContractFunctionParameters().addString("Bob").addUint256(2222))
     const contractInstantiateSubmit = await contractInstantiateTx.execute(client)
     const contractInstantiateRx = await contractInstantiateSubmit.getReceipt(client)
     const contractId = contractInstantiateRx.contractId
-    const contractAddress = contractId.toSolidityAddress()
-    //console.log(`The contract ID is: ${contractId} \n`)
+    //const contractAddress = contractId.toSolidityAddress()
+    console.log(`The contract ID is: ${contractId} \n`)
     //console.log(`The contract ID in Solidity is: ${contractAddress} \n`)
     return contractId
 }
@@ -62,7 +63,7 @@ async function queryChanges(_contractId, _name)
         .setFunction("getMobileNumber", new ContractFunctionParameters().addString(_name))
     const contractQuerySubmit = await contractQueryTx.execute(client)
     const contractQueryRx = contractQuerySubmit.getUint256(0)
-    //console.log(`Here's the phone number you asked for: ${contractQueryRx} \n`)
+    console.log(`Here's the phone number you asked for: ${contractQueryRx} \n`)
     return contractQueryRx
 }
 
@@ -77,7 +78,7 @@ async function stateVariableUpdate(_contractId, _name, _phone)
         .setMaxTransactionFee(new Hbar(.75))
     const contractExecuteSubmit = await contractTransactionTx.execute(client)
     const contractExecuteRx = await contractExecuteSubmit.getReceipt(client)
-    //console.log(`Function call Status: ${contractExecuteRx.status}`)
+    console.log(`Function call Status: ${contractExecuteRx.status}`)
     return contractExecuteRx
 }
 
